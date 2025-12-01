46d3d1dd5fe57f913dfcd17ed4b611dbf531cfd2
thinkingGorilla <4zxzxz4@gmail.com>
2025-12-01 16:30:36 +0900

    프록시 패턴 주석 추가

--- Chapter07/02/model/observable.js
+++ Chapter07/02/model/observable.js
@@ -33,11 +33,14 @@ export default (model, stateGetter) => {
     addChangeListener
   }
 
+  // model에 대한 프록시 객체 생성
   return Object
     .keys(model)
+    // 객체에서 함수를 찾는다
     .filter(key => {
       return typeof model[key] === 'function'
     })
+    // 모델의 함수를 wrapAction으로 감싼 함수를 하나씩 프록시 객체에 넣어 모델과 동일한 인터페이스를 갖도록 한다.
     .reduce((proxy, key) => {
       const action = model[key]
       return {
